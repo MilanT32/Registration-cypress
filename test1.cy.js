36755e805f92cb376ab647371f87cdee6bb16892
MilanT32 <mtmtemerin@gmail.com>
2023-03-21 15:29:07 +0200

    First commit

---
+++ test1.cy.js
@@ -0,0 +1,23 @@
+// describe('Login page', () => {
+
+//     it('Visit login page', () => {
+//         cy.visit('https://gallery-app.vivifyideas.com/login');
+//         cy.get('input[id="email"]').clear();
+//         cy.get('input[id="password"]').clear();
+//     })
+// })
+// it('Positive case - Successful login', () => {
+//     cy.visit('https://gallery-app.vivifyideas.com/login');
+//     cy.get('input[id="email"]').type('fizickiradnici23@gmail.com');
+//     cy.get('input[ id="password"]').type('milantintor23');
+//     cy.get('button[type="submit"]').click();
+// });
+// it('Logout', () => {
+//     cy.visit('https://gallery-app.vivifyideas.com/login');
+//     cy.get('input[id="email"]').type('markoqa13@gmail.com');
+//     cy.get('input[id="password"]').type('Marko123');
+//     cy.get('button[type="submit"]').click();
+//     cy.wait(3000);
+//     cy.get('a[role="button "]').click();
+// });
+
